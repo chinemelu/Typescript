844f89e5e6e7fa3dc42aa29ccf52702dc6c599c7
Chinemelu Nwosu <chinemelunwosu@gmail.com>
2025-02-13 05:46:49 +0100

    added indexed-access-types, keyof and typeof

--- src/keyof.ts
+++ src/keyof.ts
@@ -18,3 +18,15 @@ type Arrayish = {
     [n: number] : unknown
 }
 
+type A = keyof Arrayish
+
+type Mapish = {
+    [k: string]: boolean
+}
+
+type M = keyof Mapish
+
+// it's string | number in M because in JavaScript object keys are always coerced to a string
+// so obj[0] is always the same as obj["0"]
+// keyof types become especially useful when combined with mapped types, which we'll learn more
+// about later
